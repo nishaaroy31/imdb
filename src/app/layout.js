0a81c450229d91691a7ff666c9d55f6ed7ddc879
Nisha Roy <nishaaroy31@gmail.com>
2026-02-02 03:36:18 +0530

    created header

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,10 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 export const metadata = {
   title: "IMDb Clone",
-  description: "This is the IMDb clone nisha",
+  description: "This is the IMDb clone",
 };
 
 export default function RootLayout({ children }) {
@@ -14,7 +15,15 @@ export default function RootLayout({ children }) {
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
       </head>
 
-      <body>{children}</body>
+      <body>
+        {/* Header */}
+        <Header />
+
+        {/* Navbar */}
+        {/* SearchBox */}
+
+        {children}
+      </body>
     </html>
   );
 }
