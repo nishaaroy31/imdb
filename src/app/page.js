f70ea3df1e875d7fab1fec4958e9fa327eb969b6
Nisha Roy <nishaaroy31@gmail.com>
2026-02-02 02:33:38 +0530

    installed tailwind css

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <h1>HOME</h1>
-  );
+  return <h1 className="text-3xl font-bold underline text-red-500">HOME</h1>;
 }
